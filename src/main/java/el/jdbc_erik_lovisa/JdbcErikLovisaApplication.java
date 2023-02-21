541cc9ed7a0534f6b609663cde490c02f9d4d710
Erik Brandt <erikdbrandt@gmail.com>
2023-02-21 12:54:56 +0100

    setup

--- src/main/java/el/jdbc_erik_lovisa/JdbcErikLovisaApplication.java
+++ src/main/java/el/jdbc_erik_lovisa/JdbcErikLovisaApplication.java
@@ -1,6 +1,6 @@
 package el.jdbc_erik_lovisa;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,8 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JdbcErikLovisaApplication implements ApplicationRunner {
 
 
-    @Autowired
-    PostGradDOA postGradDOA;
+
 
     public static void main(String[] args) {
 
@@ -24,6 +23,7 @@ public class JdbcErikLovisaApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        postGradDOA.getAllArtists();
+
+
     }
 }
