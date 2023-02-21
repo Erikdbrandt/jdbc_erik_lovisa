628ca0468e7c3eca2a80da6bca76031c233caee9
Erik Brandt <erikdbrandt@gmail.com>
2023-02-21 09:25:38 +0100

    setup

--- src/main/java/el/jdbc_erik_lovisa/JdbcErikLovisaApplication.java
+++ src/main/java/el/jdbc_erik_lovisa/JdbcErikLovisaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JdbcErikLovisaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JdbcErikLovisaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcErikLovisaApplication.class, args);
+    }
 
 }
--- src/test/java/el/jdbc_erik_lovisa/JdbcErikLovisaApplicationTests.java
+++ src/test/java/el/jdbc_erik_lovisa/JdbcErikLovisaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JdbcErikLovisaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
