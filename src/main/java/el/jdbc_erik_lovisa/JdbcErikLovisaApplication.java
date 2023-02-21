95b5921d9c340586f09fc870f2f46a4f0ffdc400
Erik Brandt <erikdbrandt@gmail.com>
2023-02-21 09:13:44 +0100

    Initial commit

---
+++ src/main/java/el/jdbc_erik_lovisa/JdbcErikLovisaApplication.java
@@ -0,0 +1,13 @@
+package el.jdbc_erik_lovisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcErikLovisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcErikLovisaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/el/jdbc_erik_lovisa/JdbcErikLovisaApplicationTests.java
@@ -0,0 +1,13 @@
+package el.jdbc_erik_lovisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcErikLovisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
