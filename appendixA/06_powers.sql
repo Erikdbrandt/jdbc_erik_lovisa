c0acd0ec550eabfd45807ba54a97750a45a305d6
LBohman <lovisabo@hotmail.com>
2023-02-23 09:21:29 +0100

    Add appendixA with SQL scripts

---
+++ appendixA/06_powers.sql
@@ -0,0 +1,20 @@
+INSERT INTO power(name, description)
+VALUES('Air', 'Lorem ipsum dolar eu');
+
+INSERT INTO power(name, description)
+VALUES('Fire', 'Lorem ipsum dolar eu');
+
+INSERT INTO power(name, description)
+VALUES('Water', 'Lorem ipsum dolar eu');
+
+INSERT INTO power(name, description)
+VALUES('Earth', 'Lorem ipsum dolar eu');
+
+INSERT INTO superhero_power(superhero_id, power_id)
+VALUES(1, 4);
+
+INSERT INTO superhero_power(superhero_id, power_id)
+VALUES(1, 1);
+
+INSERT INTO superhero_power(superhero_id, power_id)
+VALUES(2, 4);
